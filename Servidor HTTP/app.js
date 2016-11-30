f3fc2d0ab5418022158a82609adf31be0cb53ae6
vengatus <juanque@outlook.com>
2016-11-30 13:58:05 -0500

    GET y POSTMAN

--- Servidor HTTP/app.js
+++ Servidor HTTP/app.js
@@ -1,10 +1,22 @@
-var express = require('express');
-var app = express();
+var express = require('express'); //importamos el paquete express
+var app = express(); //ejecutamos la funcion express
 
 app.get('/TecnologiasWeb', function (req, res) {
-  res.send('con javascript!');
+  //request => req  - que es lo que me manda el cliente
+  //response => res
+  res.send('con javascript! (get)');
+});
+
+app.post('/TecnologiasWeb', function (req, res) {    
+    console.log('REQUEST:');
+    console.log(req.headers);
+    console.log('RESPONSE:');
+    console.log(res.headers);
+    
+  res.send('con post!');    
 });
 
 app.listen(5050, function () {
   console.log('Example app listening on port 5050!');
 });
+
