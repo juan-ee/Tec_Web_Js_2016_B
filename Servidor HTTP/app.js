cf0bf3df52dce880749849107bdeaa26add2f31c
vengatus <juanque@outlook.com>
2016-11-30 14:05:57 -0500

    Post con Objetos

--- Servidor HTTP/app.js
+++ Servidor HTTP/app.js
@@ -2,21 +2,40 @@ var express = require('express'); //importamos el paquete express
 var app = express(); //ejecutamos la funcion express
 
 app.get('/TecnologiasWeb', function (req, res) {
-  //request => req  - que es lo que me manda el cliente
-  //response => res
-  res.send('con javascript! (get)');
+    //request => req  - que es lo que me manda el cliente
+    //response => res
+    res.send('con javascript! (get)');
 });
 
-app.post('/TecnologiasWeb', function (req, res) {    
+app.get('/', function (req, res) {
+    //request => req  - que es lo que me manda el cliente
+    //response => res
+    res.send('Hola mundo! (get)');
+});
+
+
+app.post('/TecnologiasWeb', function (req, res) {
     console.log('REQUEST:');
     console.log(req.headers);
-    console.log('RESPONSE:');
+
+
+    //res.send('con post!');    
+    //No se pueden enviar dos respuestas, NO vale poner otro send()
+
+    //Para responder objetos: 
+
+    var usuario = {
+        nombre: 'Juan',
+        cedula: '1718269671'
+    }
+
+    res.json(usuario);
+
+        console.log('RESPONSE:');
     console.log(res.headers);
-    
-  res.send('con post!');    
+
 });
 
 app.listen(5050, function () {
-  console.log('Example app listening on port 5050!');
+    console.log('Example app listening on port 5050!');
 });
-
