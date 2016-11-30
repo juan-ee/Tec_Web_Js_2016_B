4ff42d4dafc3795d805e21c7616f28fb3730a813
vengatus <juanque@outlook.com>
2016-11-30 13:40:02 -0500

    Paquete express, servidor puerto5050

---
+++ Servidor HTTP/app.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var app = express();
+
+app.get('/TecnologiasWeb', function (req, res) {
+  res.send('con javascript!');
+});
+
+app.listen(5050, function () {
+  console.log('Example app listening on port 5050!');
+});
