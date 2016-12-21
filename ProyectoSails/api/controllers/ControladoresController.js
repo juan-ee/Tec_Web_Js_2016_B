dcbfdd07443af02e3c2581e977bd5ebc265fe9b6
vengatus <juanque@outlook.com>
2016-12-21 13:40:29 -0500

    modelos

---
+++ ProyectoSails/api/controllers/ControladoresController.js
@@ -0,0 +1,11 @@
+/**
+ * ControladoresController
+ *
+ * @description :: Server-side logic for managing Controladores
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+};
+
--- ProyectoSails/api/models/MiModelo.js
+++ ProyectoSails/api/models/MiModelo.js
@@ -1,5 +1,5 @@
 /**
- * MiModelo.js
+ * MiModelo.js (ADMINISTRADOR)
  *
  * @description :: TODO: You might write a short summary of how this model works and what it represents here.
  * @docs        :: http://sailsjs.org/documentation/concepts/models-and-orm/models
