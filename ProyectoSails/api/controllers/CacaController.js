90cf19952bc5b4610855c90172bc3dee61a28a74
vengatus <juanque@outlook.com>
2017-01-05 15:42:22 -0500

    commit olvidado

---
+++ ProyectoSails/api/controllers/CacaController.js
@@ -0,0 +1,11 @@
+/**
+ * CacaController
+ *
+ * @description :: Server-side logic for managing Cacas
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+};
+
---
+++ ProyectoSails/api/controllers/EjemploApiController.js
@@ -0,0 +1,11 @@
+/**
+ * EjemploApiController
+ *
+ * @description :: Server-side logic for managing Ejemploapis
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+};
+
