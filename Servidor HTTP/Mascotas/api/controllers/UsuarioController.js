d599c5100904c95a90a7b8f35702cbf6e0497933
vengatus <juanque@outlook.com>
2017-01-10 14:14:18 -0500

    con parametros

--- Servidor HTTP/Mascotas/api/controllers/UsuarioController.js
+++ Servidor HTTP/Mascotas/api/controllers/UsuarioController.js
@@ -8,17 +8,27 @@
 module.exports = {
     crearUsuario: function (req, res) {
         //se accede /Usuario/crearUsuario
-        Usuario.create({
-            nombres: 'Finn Jose',
-            apellidos: 'Icaza',
-            correo: 'asdad'
-        }).exec(function (err, usuarioCreado) {
-            if (err) {
-                return res.serverError(err);
-            }
-            sails.log('Usuario creado\'s ', usuarioCreado);
-            return res.ok(usuarioCreado);
-        });
+        var parametros = req.allParams();
+        sails.log(parametros);
+
+        if (parametros.nombres && parametros.apellidos) {
+            Usuario.create({
+                nombres: parametros.nombres,
+                apellidos: parametros.apellidos,
+                correo: parametros.correo
+            }).exec(function (err, usuarioCreado) {
+                if (err) {
+                    return res.serverError(err);
+                }
+                sails.log('Usuario creado\'s ', usuarioCreado);
+                return res.ok(usuarioCreado);
+            });
+        } else {
+            return res.badRequest('No envias todos los parametros');
+        }
+
+
+
     }
 
 };
