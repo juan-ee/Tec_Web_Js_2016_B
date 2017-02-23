4e937dc210f38f189c128d925025e5306cdfc614
vengatus <juanque@outlook.com>
2017-02-23 17:53:18 -0500

    vistas

--- AplicacionEjemplo/src/app/app.component.ts
+++ AplicacionEjemplo/src/app/app.component.ts
@@ -1,12 +1,17 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title: string = "Hola Amigos";
+export class AppComponent implements OnInit { //OnInit es el evento que muestra cuando esta listo el componente
+  title: string = "Hola amigos";
+  nombre: string;
+  apellido: string;
+  colorbg:="red";
+  tamanoh4="51px";
+
   //--nombre: string;
   //despues de los dos puntos escribimos el tipo de datp
   //se puede definir si es privado, por defecto es publico
@@ -14,15 +19,25 @@ export class AppComponent {
   // en typescript ya no es necesaio escribir la palabra function para la funcion
   //tambien se puede escribir el modificador de acceso: void, public etc
   //tambien se puede devolver valores
-  constructor(private nombre: string, private apellido: string) {
-    this.saludar();
-    this.saludar2("Eguez");
-  }
-  saludar(): string {
-    return `Hola, mi apellido es: ${this.apellido}`;
-  }
-  saludar2(nombre: string, apellido?: string){
-    console.log(`${apellido} y ${nombre}`);
+  constructor() {
+    this.apellido = "Erazo";
+    this.nombre = "Juan";
   }
 
+  //PRIMERO SE EJECUTA EL CONSTRUCTOR Y LUEGO EL ONINIT()
+  ngOnInit() {
+    console.log("en el init");
+    this.apellido = "Sanchez";
+    this.nombre = "Enrique";
+  }
+
+  nombreCompleto2(): string {
+    return this.apellido + " " + this.nombre;
+  }
+  hizoClick(){
+    alert("hizo click");
+  }
+  mouseEncima(){
+    alert("mouse encima!");
+  }
 }
