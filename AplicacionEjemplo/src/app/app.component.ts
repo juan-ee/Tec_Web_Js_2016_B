7f5009ee9dd9ec5199a97547e2a59c32d34992e2
vengatus <juanque@outlook.com>
2017-02-23 17:08:11 -0500

    funciones que no valieron

--- AplicacionEjemplo/src/app/app.component.ts
+++ AplicacionEjemplo/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +6,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  title: string = "Hola Amigos";
+  //--nombre: string;
+  //despues de los dos puntos escribimos el tipo de datp
+  //se puede definir si es privado, por defecto es publico
+  //--apellido: string;
+  // en typescript ya no es necesaio escribir la palabra function para la funcion
+  //tambien se puede escribir el modificador de acceso: void, public etc
+  //tambien se puede devolver valores
+  constructor(private nombre: string, private apellido: string) {
+    this.saludar();
+    this.saludar2("Eguez");
+  }
+  saludar(): string {
+    return `Hola, mi apellido es: ${this.apellido}`;
+  }
+  saludar2(nombre: string, apellido?: string){
+    console.log(`${apellido} y ${nombre}`);
+  }
+
 }
--- README.md
+++ README.md
@@ -6,7 +6,7 @@
 ### webcomponents.org para reautilizar componentes web
 ### polymer para componentes web hechos por google
 
-
+# ANGULAR
 ### carpeta e2e es para las pruebas
 
 ### carpeta SRC contiene la aplicacion
@@ -15,3 +15,13 @@ se cambian variables tanto en desarrollo como en producción
 
 ### -> carpeta APP
 app.module <- es como un aplicacion, sse definen la mayoria de cosas de la aplicacion, se importan los componentes, servicios, etc
+
+#### app.module.ts --> ahi se especifican los modulos de la aplicacion
+
+### DECORATOR
+@Decorador({...})
+- Utiliza el angular para aumentar la funcionalidad del typescript
+- adentro se escribe un objeto Json con las configuraciones del objeto
+- un decorador no es parte del typescript, es de ANGULAR
+Todo en el angular dos son componentes:
+- el primero de todos es AppComponent y está en la raíz de todo
